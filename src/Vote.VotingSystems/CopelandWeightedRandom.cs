697b4f6665fbbe44222f72c2cf17d0bca8621fa2
Adam Sasine <adam.sasine@gmail.com>
2020-03-21 13:15:39 -0700

    Added comments for the random sampling algorithm

--- src/Vote.VotingSystems/CopelandWeightedRandom.cs
+++ src/Vote.VotingSystems/CopelandWeightedRandom.cs
@@ -16,24 +16,28 @@ namespace Vote.VotingSystems
 
         public override IReadOnlyCollection<Result> GetRankedResults(ISet<Candidate> candidates, IEnumerable<IEnumerable<Candidate>> votes)
         {
-            // result.Net values are in range [-n+1, n-1]
+            // use the Alias method to sample from a discrete distribution
+            // the discrete distribution is found by converting the net score of each result into a weight
+            // copeland's methodd yields net scores in the range [-n+1, n-1] where n is the number of candidates
+            // convert this to a discrete distribution of [1, 2*n-1]
             var copelandResults = base.GetRankedResults(candidates, votes).ToList();
-
-            // convert this to a discrete distribution then sample that distribution
             var n = candidates.Count();
             var probabilities = copelandResults
                 .Select(result => result.Net)
                 .Select(net => net + n)
                 .ToList();
 
-
+            // create the distribution by repeating each candidate X times, X being its weight
+            // then sample that distribution by iterating through the distribution by a random amount
+            // the easiest common denominator to find is the sum of all weights because if you divided
+            //  each element in probabilities by the sum of all probabilities, the new sum would be exactly 1
+            // potential performance improvement: find a smaller common denominator allowing for less iterations when sampling
             var sum = probabilities.Sum();
             var distribution = copelandResults
                 .Zip(probabilities, (result, probability) => (result, probability))
                 .SelectMany(pair => Enumerable.Repeat(pair.result, pair.probability));
 
-            // range [0, sum) (exclusive)
-            var randomIndex = this.random.Next(sum);
+            var randomIndex = this.random.Next(sum); // this is exclusive
             var winner = distribution.ElementAt(randomIndex);
 
             return new Result[] { winner };
